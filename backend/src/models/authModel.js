adc3edb01fd36a15e364572399c257b2c6eed82d
mohdbinharun56 <arahaman461@gmail.com>
2025-03-08 08:22:28 +0600

    [Backend]: updated login user service in authModules

--- backend/src/models/authModel.js
+++ backend/src/models/authModel.js
@@ -3,32 +3,33 @@ import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 
 const jwtSecret = process.env.JWT_SECRET || "defaultSecret";
-export const createUserService = async(user) =>{
-    const {name,email,password_hash,skills,causes} = user;
-    const bycriptPassword = await bcrypt.hash(password_hash,10);
+export const createUserService = async (user) => {
+    const { name, email, password_hash, skills, causes } = user;
+    const bycriptPassword = await bcrypt.hash(password_hash, 10);
     const result = await pool.query(`
         INSERT INTO users (name,email,password_hash,skills,causes) 
         VALUES ($1,$2,$3,$4,$5) RETURNING *`,
-        [name,email,bycriptPassword,skills,causes]
+        [name, email, bycriptPassword, skills, causes]
     );
     return result.rows;
 }
 
 // Signin user 
-export const signInUserService = async(userCredentials) =>{
-    const {email,password} = userCredentials;
-    const isUserExist = await pool.query("SELECT * FROM users where email=$1",[email]);
-    if(!isUserExist){
+export const signInUserService = async (userCredentials) => {
+    const { email, password } = userCredentials;
+    const isUserExist = await pool.query("SELECT * FROM users where email=$1", [email]);
+    if (isUserExist.rows.length === 0) {
         // Incorrect user credentials 
         throw new Error("Invalid user credentials!");
     }
+
     const user = isUserExist.rows[0];
-    const isMatch = await bcrypt.compare(password,user.password_hash);
-    if(!isMatch){
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+    if (!isMatch) {
         // Incorrect user credentials
         throw new Error("Invalid user credentials!");
     }
-    const token = jwt.sign({id: user.id},jwtSecret,{expiresIn: "1d"});
+    const token = jwt.sign({ id: user.id }, jwtSecret, { expiresIn: "1d" });
 
     return {
         user,
