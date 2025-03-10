bd1b359777a6e65ade8e89da2edeb8fac28f6858
mohdbinharun56 <arahaman461@gmail.com>
2025-03-10 15:40:08 +0600

    [Backend]: implement upcoming events shown in users only all events shown in admin

--- frontend/src/pages/Events/Events.jsx
+++ frontend/src/pages/Events/Events.jsx
@@ -1,34 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import EventsCard from '../../components/EventsCard/EventsCard';
+import { CreateAuth } from '../../components/AuthProvider/AuthProvider';
 
 const Events = () => {
+    const { user } = useContext(CreateAuth);
+
     const loadedEvents = useLoaderData();
     console.log(loadedEvents.data);
-    const [events, setEvents] = useState(loadedEvents.data);
+    const [events, setEvents] = useState([]);
     const [educationCategories, setEducationCategories] = useState([]);
     const [environmentCategories, setEnvironmentCategories] = useState([]);
     const [healthcareCategories, setHealthcareCategories] = useState([]);
+
+
+    useEffect(() => {
+        const today = new Date().toISOString().split("T")[0]; // YYYY-MM-DD
+
+        if (user.role === "admin") {
+            setEvents(loadedEvents.data);
+        } else {
+            const upcomingEvents = loadedEvents.data.filter(event => event.date >= today);
+            setEvents(upcomingEvents);
+        }
+    }, [loadedEvents.data, user.role]); 
 
     useEffect(() => {
         const categoryEducation = events?.filter(event => event.category === 'Education');
         console.log(categoryEducation);
         setEducationCategories(categoryEducation);
-    }, [])
+    }, [events])
 
     useEffect(() => {
         const categoryEnvironment = events?.filter(event => event.category === 'Environment');
         console.log(categoryEnvironment);
         setEnvironmentCategories(categoryEnvironment);
-    }, [])
+    }, [events])
 
     useEffect(() => {
         const categoryHealthcare = events?.filter(event => event.category === 'Healthcare');
         console.log(categoryHealthcare);
         setHealthcareCategories(categoryHealthcare);
-    }, [])
+    }, [events])
 
     return (
         <div className='lg:w-1/2 mx-auto'>
@@ -39,6 +54,7 @@ const Events = () => {
                     <Tab>Environment</Tab>
                     <Tab>Healthcare</Tab>
                 </TabList>
+
 
                 <TabPanel>
                     {
